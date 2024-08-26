fe8dd994cb9848de1ccb4667519260b0d74bab4e
aakoshy <aakoshy@ad.unc.edu>
2024-08-26 14:33:36 -0400

    Finished Exercise 00!

---
+++ #example.py
@@ -0,0 +1,10 @@
+# example.py
+print(float(3.7))
+print("h3ll0"[2 + 1])
+print({1_000 + 1_000_000})
+print([10 / 2])
+print("U" + "N" + "C")
+print("12345"[0])
+print("spooky season"[2 + 2])
+
+print("spooky"[len("spooky")])
